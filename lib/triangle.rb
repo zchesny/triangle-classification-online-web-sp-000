537ca2a8af6e922d6c8dd54fb976a475112295ed
Zoey Chesny <z.chesny@gmail.com>
2020-04-16 16:21:28 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,24 +1,13 @@
+# refactored based on solution 
 class Triangle
   # write code here
-  attr_reader :l1, :l2, :l3
+  attr_reader :a, :b, :c
 
-  def initialize(l1, l2, l3)
-    traingle_inequality = [
-      l1 > 0,
-      l2 > 0,
-      l3 > 0,
-      l1 + l2 > l3,
-      l2 + l3 > l1,
-      l1 + l3 > l2
-    ]
-    if traingle_inequality.all?{|condition| condition}
-      @l1 = l1
-      @l2 = l2
-      @l3 = l3
-    else
-      raise TriangleError
-    end
-  end
+  def initialize(a, b, c)
+    @a = a
+    @b = b 
+    @c = c 
+  end 
 
   def kind
     if l1 == l2 && l2 == l3
