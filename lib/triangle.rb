6bf94db3fb4fa4a0d782c2d337f3fbddf2c06190
Zoey Chesny <z.chesny@gmail.com>
2020-04-16 16:18:32 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,35 @@
 class Triangle
   # write code here
+  attr_reader :l1, :l2, :l3
+
+  def initialize(l1, l2, l3)
+    traingle_inequality = [
+      l1 > 0,
+      l2 > 0,
+      l3 > 0,
+      l1 + l2 > l3,
+      l2 + l3 > l1,
+      l1 + l3 > l2
+    ]
+    if traingle_inequality.all?{|condition| condition}
+      @l1 = l1
+      @l2 = l2
+      @l3 = l3
+    else
+      raise TriangleError
+    end
+  end
+
+  def kind
+    if l1 == l2 && l2 == l3
+      return :equilateral
+    elsif l1 == l2 || l2 == l3 || l1 == l3
+      return :isosceles
+    else
+      return :scalene
+    end
+  end
+
+  class TriangleError < StandardError
+  end
 end
